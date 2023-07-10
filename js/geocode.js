4ef991e42fb90074293ed37c3dff8b2eb60ad672
sobbaka <a.bagaev1989@gmail.com>
2023-07-10 23:11:22 +0400

    add rendering via browser + error handle

--- js/geocode.js
+++ js/geocode.js
@@ -1,39 +1,62 @@
 import { getToken } from './secret.js'
 
+const searchForm = document.querySelector('form')
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  createPlaceAnswer()
+})
+
 async function getLocation(lat, long) {
   const place = await fetch(`https://geocode.xyz/${lat},${long}?geoit=json&auth=${getToken()}`)
     .then(data => data.json())
     .then(data => {
       console.log(data)
-      if (data.hasOwnProperty('success') && !data.success) throw new Error(data.error.message)
+      if (data.hasOwnProperty('error')) {
+        const errorMessage = data.error.message ? data.error.message : data.error.description
+        console.log(errorMessage)
+        throw new Error(errorMessage)
+      }
       return data
     }).catch(e => {
-      console.log(e)
-      return { text: e, err: true }
+      return { text: e.message, err: true }
     })
-
   return place
 }
 
-function renderPosition(country, city) {
-  // работает в случае ошибки
+
+function getLatAndLong() {
+  const lat = document.querySelector('#latitude').value
+  const long = document.querySelector('#longitude').value
+  return [lat, long]
+}
+
+async function createPlaceAnswer() {
+  const [lat, long] = await getLatAndLong()
+  console.log(lat, long)
+  const data = await getLocation(lat, long)
+  await renderAnswer(data)
+}
+
+function renderPosition(data) {
+
   const answer = document.querySelector('.answer')
-  const lowerCity = city ? city.charAt(0).toUpperCase() + city.slice(1).toLowerCase() : ''
-  const position = lowerCity ? `${lowerCity}, ${country}` : country
+  const lowerCity = data.city ? data.city.charAt(0).toUpperCase() + data.city.slice(1).toLowerCase() : ''
+  const country = data.country ? data.country : ''
+  const position = [lowerCity, country].length ? [lowerCity, country].filter(str => str.length > 0).join(',') : 'неизвестном месте'
+  const btnHTML = data.prov ? `<button type="button" class="place__button" data-country="${data.prov}">Подробнее</button>` : ''
+
+
   const html = `
         <div class="place">
           <h2 class="place__title">
             Вы находитесь в<br>
             ${position}
           </h2>
-          <button type="button" class="place__button" data-country="${country}">Подробнее</button>
+            ${btnHTML}
         </div>
       `
   answer.innerHTML = html
 }
-
-const place = await getLocation(51.50354, -0.12768)
-renderAnswer(place)
 
 
 function renderError(error) {
@@ -46,13 +69,21 @@ function renderError(error) {
   answer.innerHTML = html
 }
 
+
 function renderAnswer(data) {
   if (data.hasOwnProperty('err')) {
     renderError(data.text)
     return
   }
-  renderPosition(data.country, data.city)
+  renderPosition(data)
 }
+
+
+
+
+// const place = await getLocation(51.50354, -0.12768)
+// renderAnswer(place)
+
 
 // getLocation(51.50354, -0.12768)
 // await getLocation(51.50354, -0.12768)
